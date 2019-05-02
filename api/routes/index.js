7221e5f862b74bbddafb291a978015f5a53b12e4
aribalam <aribalam64@gmail.com>
2019-05-02 22:13:12 +0530

    add few endpoints

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.send("This is the home page");
+});
+
+router.get('/search', (req, res) => {
+  res.send("This is the search form");
+});
+
+router.get('/login', (req, res) => {
+  res.send("This is the login page");
 });
 
 module.exports = router;
