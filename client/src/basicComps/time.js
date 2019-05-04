2c6ebd0a76ab8464e1bb7fe0d4eae31cf7da5aa5
rka87338 <rka87338@gmail.com>
2019-05-04 19:49:17 +0530

    succesfully implemented time of material ui picker

---
+++ client/src/basicComps/time.js
@@ -0,0 +1,47 @@
+import 'date-fns';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, TimePicker} from 'material-ui-pickers';
+
+const styles = {
+  grid: {
+    width: '60%',
+  },
+};
+
+class MaterialUIPickers extends React.Component {
+  state = {
+    // The first commit of Material-UI
+    selectedDate:  Date('2014-08-18T21:11:54'),
+  };
+
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+    var selectedTime = String(date).split(' ')[4]
+    console.log(selectedTime)
+  };
+
+  render() {
+    const { selectedDate } = this.state;
+
+    return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <TimePicker
+            margin="normal"
+            label="Time picker"
+            value={selectedDate}
+            onChange={this.handleDateChange}
+            
+          />
+         </MuiPickersUtilsProvider>
+    );
+  }
+}
+
+MaterialUIPickers.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MaterialUIPickers);
--- client/src/comps/search.js
+++ client/src/comps/search.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from '../basicComps/select'
 import DateSelect from '../basicComps/date'
+import TimeSelect from '../basicComps/time'
 import  '../styles/App.scss'
 
 function Search (){
@@ -10,6 +11,7 @@ function Search (){
                 <h2>From</h2><Menu /><h2>To</h2><Menu />
             </div>
             <DateSelect />
+            <TimeSelect />
         </div>
     )
 }
