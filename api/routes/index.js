16e11b464b4720c0ccba560c94292dd36527ce43
aribalam <aribalam64@gmail.com>
2019-05-04 22:20:36 +0530

    implemented few endpoints

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 var models = require('../models/index').models;
 
-/* GET home page. */
 router.get('/', (req, res) => {
-  res.send("This is the home page");
+  models.Group.find(req.query, { __v: 0}).exec((err, objects) => {
+    res.send(objects);
+  });
 });
 
-router.get('/search', (req, res) => {
-  res.send("This is the search form");
+router.get('/group/:id', (req, res) => {
+  models.Group.findOne({_id: req.params.id}, {_id: 0, __v: 0}).exec((err, object) => {
+    res.send(object);
+  });
+});
+
+router.post('/search', (req, res) => {
+  res.redirect(301, url.format({
+    pathname: '/',
+    query: req.body,
+  }));
 });
 
 router.get('/login', (req, res) => {
