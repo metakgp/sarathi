897abfeca5f304517c70c4259664f9f9b3d0ab71
aribalam <aribalam64@gmail.com>
2019-05-06 02:34:13 +0530

    added enpoints for creating and joining group

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,14 +4,67 @@ var url = require('url');
 var models = require('../models/index').models;
 
 router.get('/', (req, res) => {
-  models.Group.find(req.query, { __v: 0}).exec((err, objects) => {
+  models.Group.find({}, {__v: 0}).exec((err, objects) => {
     res.send(objects);
   });
 });
 
-router.get('/group/:id', (req, res) => {
-  models.Group.findOne({_id: req.params.id}, {_id: 0, __v: 0}).exec((err, object) => {
-    res.send(object);
+router.get('/create_group', (req, res) => {
+  var username = get_username();
+  var traveler = {
+    name: username,
+    from: req.query.from,
+    to: req.query.to,
+    time: req.query.time,
+  };
+  var grp = models.Group({
+    from: req.query.from,
+    to: req.query.to,
+    owner: traveler,
+    members: [traveler],
+    departure: req.query.time,
+    status: 'open',
+  });
+  grp.save((err) => {
+    if (err)
+      res.send(500, "error creating group");
+    else
+      res.send(200, "OK");
+  });
+});
+
+router.get('/join_request/:id', (req, res) => {
+  var username = get_username();
+  var traveler = {
+    name: username,
+    from: req.query.from,
+    to: req.query.to,
+    time: req.query.time,
+  };
+  models.Group.findById(req.params.id).exec((err, object) => {
+    var req = models.Request({
+      group: object,
+      traveler: traveler,
+    });
+    req.save((err) => {
+      if (err)
+        res.send(500, "Error creating request");
+      else {
+        // add those requests to the users concerned
+        models.User.findOneAndUpdate({name: username}, {$push: {sent_requests: req}})
+        .exec((err, object) => {
+          if (err)
+            res.send(500, "Error adding sent request to user");
+        });
+        models.User.findOneAndUpdate({name: object.owner.name}, {$push: {sent_requests: req}})
+        .exec((err, object) => {
+          if (err)
+            res.send(500, "Error adding received request to user");
+          else
+            res.send(200, "OK");
+        });
+      }
+    });
   });
 });
 
@@ -25,5 +78,6 @@ router.post('/search', (req, res) => {
 router.get('/login', (req, res) => {
   res.send("This is the login page");
 });
+
 
 module.exports = router;
