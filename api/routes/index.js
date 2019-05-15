1da9826197426d17ae010951d1e21353f9ae03d9
aribalam <aribalam64@gmail.com>
2019-05-15 16:55:32 +0530

    implemented notification for approve and reject requests

--- api/routes/index.js
+++ api/routes/index.js
@@ -95,35 +95,64 @@ router.post('/join_request', (req, res) => {
 // approves a request to join the group
 //TODO: update notification and send it to all members
 router.post('/approve_request', (req, res) => {
-  var traveler = {
-    fb_id: req.body.fb_id,
-    name: req.body.name,
-    from: req.body.from,
-    to: req.body.to,
-    time: req.body.time,
-  };
-  models.Group.findByIdAndUpdate(req.body.groupId, {$push: {members: traveler}})
-  .exec((err, object) => {
+  // find the request object
+  models.Request.findById(req.body.requestId, (err, request) => {
     if (err)
       res.send(500, err);
     else {
-      // remove request from sent_request of user
-      models.User.findOneAndUpdate({name: req.body.fb_id}, 
-        {$pull: {sent_requests: req.body.requestId}, $push: {joined_groups: object}})  //remove requests matching req id
-        .exec((err, object) => {
-          if (err)
-            res.send(500, "error removing request from user");
-        });
-      
-      // remove request from recieved_request from group owner
-      models.User.findOneAndUpdate({name: object.owner.fb_id}, 
-        {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
-        .exec((err, object) => {
-          if (err)
-            res.send(500, "error removing request from user");
-          else
-            res.send(200, "OK");
-        });
+      // find the group and add the travler
+      models.Group.findByIdAndUpdate(request.group, {$push: {members: request.traveler}})
+      .exec((err, group) => {
+        if (err)
+          res.send(500, err);
+        else {
+          // remove received request from owner of the group
+          models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, 
+            {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
+            .exec((err) => {
+              if (err)
+                res.send(500, "error removing request from user");
+              else {
+                // remove request from sent_request of traveler
+                models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
+                  {$pull: {sent_requests: req.body.requestId}, $push: {joined_groups: group}})  //remove requests matching req id
+                  .exec((err, traveler) => {
+                    if (err)
+                      res.send(500, "error removing request from user");
+                    else {
+                      
+                      // message to all the members of the group
+                      const message = JSON.stringify({
+                        type: 'approve_request',
+                        title: 'Group Update',
+                        body: request.traveler.name + " has joined the group",
+                      });
+                      
+                      // sending notification to all members of the group
+                      group.members.map((member) => {
+                        models.User.findOne({fb_id: member.fb_id})
+                        .exec((err, user) => {
+                          webpush.sendNotification(JSON.parse(user.push_subscription), message)
+                          .catch(err => console.log(err));
+                        });
+                      });
+
+                      // message to the traveler
+                      const message2 = JSON.stringify({
+                        type: 'approve_request',
+                        title: 'Request Update',
+                        body: group.owner.name + " has accepted your request",
+                      });
+
+                      webpush.sendNotification(JSON.parse(traveler.push_subscription), message2)
+                      .catch(err => console.log(err))
+                      .then(() => res.send(200, "OK"));
+                    }
+                  });
+              }
+            });
+        }
+      });
     }
   });
 });
@@ -131,28 +160,48 @@ router.post('/approve_request', (req, res) => {
 // rejects a request to join the group
 //TODO: update notification and send it to all members
 router.post('/reject_request', (req, res) => {
-  models.Group.findById(req.body.groupId)
-  .exec((err, object) => {
+  // find the request object
+  models.Request.findById(req.body.requestId, (err, request) => {
     if (err)
       res.send(500, err);
     else {
-      // remove request from sent_request of user
-      models.User.findOneAndUpdate({name: req.body.fb_id}, 
-        {$pull: {sent_requests: req.body.requestId}})  //remove requests matching req id
-        .exec((err, object) => {
-          if (err)
-            res.send(500, "error removing request from user");
-        });
-      
-      // remove request from recieved_request from group owner
-      models.User.findOneAndUpdate({name: object.owner.fb_id}, 
-        {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
-        .exec((err, object) => {
-          if (err)
-            res.send(500, "error removing request from user");
-          else
-            res.send(200, "OK");
-        });
+      // find the group and add the travler
+      models.Group.findById(request.group)
+      .exec((err, group) => {
+        if (err)
+          res.send(500, err);
+        else {
+          // remove received request from owner of the group
+          models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, 
+            {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
+            .exec((err) => {
+              if (err)
+                res.send(500, "error removing request from user");
+              else {
+                // remove request from sent_request of traveler
+                models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
+                  {$pull: {sent_requests: req.body.requestId}, $push: {joined_groups: group}})  //remove requests matching req id
+                  .exec((err) => {
+                    if (err)
+                      res.send(500, "error removing request from user");
+                    else {
+                      // message to the traveler
+                      const message = JSON.stringify({
+                        type: 'approve_request',
+                        title: 'Request Update',
+                        body: group.owner.name + " has rehected your request",
+                      });
+
+                      webpush.sendNotification(JSON.parse(traveler.push_subscription), message)
+                      .catch(err => console.log(err))
+                      .then(() => res.send(200, "OK"));
+                      res.send(200, "OK");
+                    }
+                  });
+              }
+            });
+        }
+      });
     }
   });
 });
