a18e725b220893df6e8a901d75aec1e8c6482059
aribalam <aribalam64@gmail.com>
2019-05-25 21:54:09 +0530

    designed the sent request card

---
+++ client/src/plans-notifs/sentRequest.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Chip from '@material-ui/core/Chip';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import moment from 'moment';
+
+import Avatar from '@material-ui/core/Avatar';
+import { CardActions, Divider } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+export default function SentRequestCard(props) {
+
+    return (
+        <Card style={{maxWidth: 500}}>
+            <Grid container style={{padding: 10}}>
+                <Grid item xs>
+                    <Typography variant='h5'>
+                    {moment(props.departure).format('Do MMMM')}
+                    </Typography>
+                    <Typography variant='subtitle2'>
+                    {props.from} to {props.to}    
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant='h5'>
+                    {moment(props.departure).format('hh:mm a')}
+                    </Typography>
+                </Grid>
+            </Grid>
+                <Divider variant='middle' />
+                    <div style={{margin: 10}}>
+                        <Chip
+                        avatar={<Avatar alt='Remy Sharp' src='http://localhost:5000/images/user-image.png'/>} 
+                        label={'Owner: ' + props.owner} 
+                        variant='outlined' />
+                        <Chip label={props.members + ' members'} variant='outlined' style={{margin:5}} />
+                        <Chip label='open' variant='outlined' style={{margin:5}} />
+                    </div>
+            <CardActions>
+                <Button>Cancel request</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+
+
