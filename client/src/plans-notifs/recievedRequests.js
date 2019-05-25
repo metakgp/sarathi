0b835303572653aff865795747c22b1486e2dcb4
aribalam <aribalam64@gmail.com>
2019-05-25 21:53:52 +0530

    designed the receieved request card

---
+++ client/src/plans-notifs/recievedRequests.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
+
+import Avatar from '@material-ui/core/Avatar';
+import { CardActions, Divider } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import moment from 'moment';
+
+export default function ReceivedRequestCard(props) {
+    
+    return (
+        <Card style={{minWidth: 500, maxWidth: 500, marginBottom: 5}}>
+            <div style={{margin: 5}}>
+                <Chip label={props.from + ' to '+props.to} 
+                variant='outlined' 
+                style={{margin:5}} />
+                <Chip label={moment(props.departure).format('Do MMMM')} 
+                variant='outlined' 
+                style={{margin:5}} />
+                <Chip label={moment(props.departure).format('hh:mm a')} 
+                variant='outlined' 
+                style={{margin:5}} />
+                <Chip label={props.members + ' members'} 
+                variant='outlined' 
+                style={{margin:5}} />
+            </div>
+            <Divider variant='middle' />
+            <div style={{padding: 10, display: 'flex', alignItems: 'center'}}>
+                <Avatar 
+                alt='Remy Sharp' 
+                src='http://192.168.0.103:5000/images/user-image.png'
+                style={{margin: 10, height: 50, width: 50}}
+                />
+                <div style={{padding: 5}}>
+                    <Typography variant='h6'>{props.traveler.name}</Typography>
+                    <Typography variant='subtitle1'>
+                        Flight Time: {moment(props.traveler.time).format('hh:mm a')}
+                    </Typography>
+                </div>
+            </div>
+            <CardActions>
+                <Button>Approve</Button>
+                <Button>Reject</Button>
+            </CardActions>
+        </Card>
+    )
+
+}
