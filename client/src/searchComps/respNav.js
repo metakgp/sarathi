1d2968c297a9103acd25320af906e54aa22dda24
rka87338 <rka87338@gmail.com>
2019-05-27 12:35:01 +0530

    material ui tyography in navbar

--- client/src/searchComps/respNav.js
+++ client/src/searchComps/respNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
- 
+import Typography from '@material-ui/core/Typography';
+
 export default class Example extends Component {
   
   myFunction = () => {
@@ -16,13 +17,33 @@ export default class Example extends Component {
     return (
       <div>
         <div className="topnav" id="myTopnav">
-                  <a href="/" >TRAVEL KGP</a>
+                  <a href="/" >
+                    <Typography variant="h6">
+                        TRAVEL KGP !
+                    </Typography>
+                  </a>
                <div className='to-right'>
                   <a href="#" id="hide">1</a>
-                  <a href="/requests">Requests</a>
-                  <a href="/groups">Groups</a>
-                  <a href="/notifications">Notifications</a>
-                  <a href="#">Logout</a>
+                  <a href="/requests">
+                    <Typography variant="h6">
+                          Requests
+                    </Typography>  
+                  </a>
+                  <a href="/groups">
+                     <Typography variant="h6">
+                          Groups
+                      </Typography>
+                   </a>
+                  <a href="/notifications">
+                    <Typography variant="h6">
+                            Notifications
+                        </Typography>
+                  </a>
+                  <a href="#">
+                      <Typography variant="h6">
+                         Logout
+                      </Typography>
+                  </a>
                
                 </div>
               <a href="javascript:void(0);" className="icon" onClick={this.myFunction}>
