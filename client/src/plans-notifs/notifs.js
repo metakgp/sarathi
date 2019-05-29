66f070293ae2d234b55b2aec5634abd814764cb8
rka87338 <rka87338@gmail.com>
2019-05-29 12:06:14 +0530

    unable to remove the padding or spacing, really furstaing will try w3 hover

--- client/src/plans-notifs/notifs.js
+++ client/src/plans-notifs/notifs.js
@@ -1,19 +1,53 @@
-import React,{ Component } from 'react'
-import List from './lists'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
-export default class Notifs extends Component {
-    render(){
-        return(
-            <div>
-                <h1 id='no-notifs-plans'>CURRENTLY NO NOTIFICATION</h1>
-               <div id='notifs'> 
-                    <List
-                    msg1='this will be some long notification which will be telling you got a request from '
-                    msg2='this will be some long notification which will be telling you got a request from '
-                    msg3='this will be some long notification which will be telling you got a request from '
-                    />
-                </div>
-            </div>
-        )
-    }
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
+
+ 
+function AlignItemsList(props) {
+  const classes = useStyles();
+
+  return (
+    <div id='notif-cards'>
+        <List className={classes.root}>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+            primary={props.msg}
+            secondary={
+                <React.Fragment>
+                <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                >
+                {props.time}
+                </Typography>
+                            </React.Fragment>
+            }
+            />
+        </ListItem>
+        </List>
+     </div>
+  );
 }
+
+export default AlignItemsList;
