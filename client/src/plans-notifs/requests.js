6cff5c4e45fe8f79d62e89a5fb67e05979f35e37
aribalam <aribalam64@gmail.com>
2019-05-31 20:00:29 +0530

    changed query param for testing

--- client/src/plans-notifs/requests.js
+++ client/src/plans-notifs/requests.js
@@ -23,7 +23,7 @@ export default class Requests extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('http://192.168.0.103:5000/user/my_requests?fb_id=2177672832321382')
+        axios.get('http://192.168.0.103:5000/user/my_requests?fb_id=2177672832321333')
         .then(res => {
             console.log(res.data.sent);
             var receivedRequestArray = res.data.received.map(item => {
