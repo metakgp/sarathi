70fab7a2a439ebc179363319090bb04d56918040
aribalam <aribalam64@gmail.com>
2019-06-01 15:21:12 +0530

    added the notification component to the page

--- client/src/pages/notifs.js
+++ client/src/pages/notifs.js
@@ -1,78 +1,58 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import React, {Component} from 'react'
+import axios from 'axios'
+import {List, Grid, ListItem, Avatar, Typography} from '@material-ui/core'
 
-const SampleNotifs = [
-  {
-    message: 'x has agreed to join u from kgp to ccu vx has agreed to join u from kgp to ccu',
-    time: '15 min ago'
-  },
-  {
-    message: 'x has agreed to join u from kgp to ccu',
-    time: '15 min ago'
-  },
-  {
-    message: 'x has agreed to join u from kgp to ccu',
-    time: '15 min ago'
-  },
-  
-]
+export default class Notifications extends Component {
+    
+    state = {
+        notifications: [],
+    }
 
+    componentDidMount() {
+        axios.get('http://192.168.0.103:5000/user/my_notifications?fb_id=2177672832321382')
+        .then((res) => {
+            this.setState({notifications: res.data});
+        })
+        .catch(err => console.log(err));
+    }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  inline: {
-    display: 'inline',
-  },
-}));
-
- 
-function AlignItemsList(props) {
-  const classes = useStyles();
-
-  return (
-    <div id='notif-cards'>
-        <List className={classes.root}>
-            {
-              SampleNotifs.map(item => {
-                return(
-                  <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                  </ListItemAvatar>
-                  <ListItemText
-                  primary={item.message}
-                  secondary={
-                      <React.Fragment>
-                      <Typography
-                          component="span"
-                          variant="body2"
-                          className={classes.inline}
-                          color="textPrimary"
-                      >
-                      {item.time}
-                      </Typography>
-                      </React.Fragment>
-                      
-                  }
-                  />
-                  </ListItem>
-
-                )
-              })
-            }
-             </List>
-     </div>
-  );
+    render() {
+        return (
+            <List>
+            {this.state.notifications.map(item =>
+                (<ListItem alignItems="flex-start">
+                <Grid container spacing={2}>
+                    <Grid item style={{display: 'flex', alignItems: 'center'}}>
+                        <Avatar 
+                        alt='Arib Alam' 
+                        src='http://192.168.0.103:5000/images/user-image.png'
+                        style={{height: 60, width: 60}} />
+                    </Grid>
+                    <Grid item xs style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                        <Typography variant='body1'>{item.message}</Typography>
+                        <Typography variant='caption'>{item.created_on}</Typography>
+                    </Grid>
+                </Grid>
+                </ListItem>)
+            )}
+            </List>
+        );
+    }
 }
 
-export default AlignItemsList;
+{/* <ListItemAvatar>
+    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+</ListItemAvatar>
+<ListItemText
+primary={item.message}
+secondary={
+<React.Fragment>
+    <Typography
+        component="span"
+        variant="body2"
+        className={classes.inline}
+        color="textPrimary"
+    >
+    {item.time}
+    </Typography>
+</React.Fragment>} /> */}
