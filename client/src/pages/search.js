215f44e5bd47daf57954e9dbe3b541e106c50cc4
aribalam <aribalam64@gmail.com>
2019-06-01 14:26:30 +0530

    redesigned the search page

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,39 +1,45 @@
 import React,{ Component } from 'react'
-import Menu from '../searchComps/select'
-import DateSelect from '../searchComps/date'
+import MaterialSelect from '../searchComps/material-select'
+import MaterialDate from '../searchComps/materialDate'
 import TimeSelect from '../searchComps/time'
-// import Card from '../plans-notifs/card'
 import Card from '../plans-notifs/card';
 import axios from 'axios';
 import  '../styles/App.scss';
-import Grid from '@material-ui/core/Grid';
-import { List, ListItem } from '@material-ui/core';
+import { Paper, Typography, Grid, Button } from '@material-ui/core';
 
 class Search extends Component{
     constructor(props){
         super(props)
         this.state = {
             fromPlace : 'KGP',
-            toPlace :'KGP',
+            toPlace :'CCU',
             time: new Date(),
             showCard: false,
             dataCards: [],
             contentSectionHeight: 0,
+            contentSectionWidth: 0,
+            contentSectionMargin: 0,
         }
-        this.updateContentSectionHeight = this.updateContentSectionHeight.bind(this);
+        this.updateContentDimensions = this.updateContentDimensions.bind(this);
     }
 
     componentDidMount() {
-        this.updateContentSectionHeight();
-        window.addEventListener('resize', this.updateContentSectionHeight);
+        this.updateContentDimensions();
+        window.addEventListener('resize', this.updateContentDimensions);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.updateContentSectionHeight);
+        window.removeEventListener('resize', this.updateContentDimensions);
     }
 
-    updateContentSectionHeight() {
-        this.setState({contentSectionHeight: window.innerHeight});
+    updateContentDimensions() {
+        const margin = document.getElementById('search_section').clientHeight;
+        const width = window.innerWidth < 500 ? window.innerWidth : 500;
+        this.setState({
+            contentSectionHeight: window.innerHeight - margin, 
+            contentSectionWidth: width,
+            contentSectionMargin: margin,
+        });
     }
     
     setFromPlace = (fromPlace) => { this.setState({fromPlace : fromPlace}) }
@@ -54,7 +60,7 @@ class Search extends Component{
         this.setState({time: newDate});
     }
     
-    sendData = () => {
+    handleSearch = () => {
         // console.log(this.state.fromPlace)
         // console.log(this.state.toPlace)
         // console.log(this.state.time)
@@ -89,52 +95,95 @@ class Search extends Component{
         });
     }
 
-    render(){
+    render() {
         
-        return(
-            <div id='homepage'>
+        return (
+        <div>
+        <Paper id='search_section' style={{position: 'fixed', top: 46, left: 0, width: '100%', zIndex: 1}}>
+            <Grid container justify='center' >
+                <Grid item xs={12} sm={6} lg={2}>
+                    <MaterialSelect dir='FROM' onPassData={this.setFromPlace} />
+                </Grid>
+                <Grid item xs={12} sm={6} lg={2}>
+                    <MaterialSelect dir='TO' onPassData = {this.setToPlace} />
+                </Grid>
+                <Grid item xs={12} sm={6} lg={3}>
+                    <MaterialDate onPassData = {this.setDate} />
+                </Grid>
+                <Grid item xs={12} sm={6} lg={3}>
+                    <TimeSelect onPassData = {this.setTime}  />
+                </Grid>
+                <Grid item xs={12} sm={12} lg={2}  style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'center'}}>
+                    <Button onClick={this.handleSearch} color='primary' size='large' style={{margin: 5}}>
+                    Search
+                    </Button>
+                </Grid>
+            </Grid>
+        </Paper>
+        <div id='card' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div style={{height: this.state.contentSectionMargin, margin: 10}}></div>
+        { this.state.showCard ?
+        this.state.dataCards.map((item, index) => {
+            return(
+                <Card
+                key={item._id}
+                id={item._id} 
+                width={this.state.contentSectionWidth}
+                departure = {item.departure}
+                from = {item.from}
+                to = {item.to}
+                status = {item.status}
+                members = {item.members}
+                join={() => this.sendJoinRequest(item._id, index)}
+                />
+            )
+        }) 
+        : ''}
+        </div>
+        </div>
+            // <div id='homepage'>
             
-                <Grid container spacing={5}>
-                <Grid item>
-                <div className="search" style={{zIndex: 1}}>
-                    <div className="fromTo">
-                        <div className="menu-des">
-                            <h2>From</h2><Menu onPassData={this.setFromPlace} />
-                        </div>
-                        <div className="menu-des">    
-                            <h2>To</h2><Menu onPassData = {this.setToPlace} />
-                        </div>    
-                    </div>
-                    <div className="selectDate">
-                        <h3>Date of Departure :</h3>
-                        <DateSelect onPassData = {this.setDate} />
-                    </div>
-                    <TimeSelect onPassData={this.setTime} />
-                    <button onClick={this.sendData}>Search</button>
-                </div>
-                </Grid>
-                <Grid item xs style={{height: this.state.contentSectionHeight, overflowY: 'scroll', overflowX: 'hidden'}}>
-                <div id='card'>
-                   {(this.state.showCard===true) ?
-                     this.state.dataCards.map((item, index) => {
-                         return(
-                             <Card
-                             key={item._id}
-                             id={item._id} 
-                             departure = {item.departure}
-                             from = {item.from}
-                             to = {item.to}
-                             status = {item.status}
-                             members = {item.members}
-                             join={() => this.sendJoinRequest(item._id, index)}
-                             />
-                         )
-                     }) 
-                     : ''}
-                </div>
-                </Grid>
-                </Grid>
-            </div>
+            //     <Grid container spacing={5}>
+            //     <Grid item>
+            //     <div className="search" style={{zIndex: 1}}>
+            //         <div className="fromTo">
+            //             <div className="menu-des">
+            //                 <h2>From</h2><Menu onPassData={this.setFromPlace} />
+            //             </div>
+            //             <div className="menu-des">    
+            //                 <h2>To</h2><Menu onPassData = {this.setToPlace} />
+            //             </div>    
+            //         </div>
+            //         <div className="selectDate">
+            //             <h3>Date of Departure :</h3>
+            //             <DateSelect onPassData = {this.setDate} />
+            //         </div>
+            //         <TimeSelect onPassData={this.setTime} />
+            //         <button onClick={this.sendData}>Search</button>
+            //     </div>
+            //     </Grid>
+            //     <Grid item xs style={{height: this.state.contentSectionHeight, overflowY: 'scroll', overflowX: 'hidden'}}>
+            //     <div id='card'>
+            //        {(this.state.showCard===true) ?
+            //          this.state.dataCards.map((item, index) => {
+            //              return(
+            //                  <Card
+            //                  key={item._id}
+            //                  id={item._id} 
+            //                  departure = {item.departure}
+            //                  from = {item.from}
+            //                  to = {item.to}
+            //                  status = {item.status}
+            //                  members = {item.members}
+            //                  join={() => this.sendJoinRequest(item._id, index)}
+            //                  />
+            //              )
+            //          }) 
+            //          : ''}
+            //     </div>
+            //     </Grid>
+            //     </Grid>
+            // </div>
         )
     }
 }
