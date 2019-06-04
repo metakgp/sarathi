1bc240fcfd8ff2acb9bb087eb08feaf8c49d7b1a
aribalam <aribalam64@gmail.com>
2019-06-04 14:20:00 +0530

    added subject to the notification model

--- api/models/notification.js
+++ api/models/notification.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const notificationSchema = mongoose.Schema({
     type: String,
     message: String,
+    subject: {
+        fb_id: String,
+        name: String,
+    }
     object_id: mongoose.Schema.Types.ObjectId,
     created_on: Date, 
     read: Boolean,
