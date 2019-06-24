cefa448daa298fc2abb72d16f9793b1c82b30c09
Arib Alam <aribalam64@gmail.com>
2019-06-24 11:41:23 +0530

    Added build files as static resources in production mode

--- app.js
+++ app.js
@@ -81,9 +81,12 @@ webpush.setVapidDetails(
   config.privateKey
 );
 
-// staticPageRouter.use((req, res) => {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
+
+if (process.env.NODE_ENV === 'production') {
+  staticPageRouter.use((req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
