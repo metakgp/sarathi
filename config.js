03f1c0590dc1ff1a02fca848c35d2308c3893dfe
Arib Alam <aribalam64@gmail.com>
2019-06-24 12:24:52 +0530

    redirect to production url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     appId:367673853841637,
     appSecret:'89d8b85e7ad8be62bef548534d6c60bb',
-    callbackUrl:'http://localhost:5000/api/auth/facebook/callback',
+    callbackUrl:'https://travelkgp.herokuapp.com/api/auth/facebook/callback',
     profileURL: 'https://graph.facebook.com/v3.3/me',
     authorizationURL: 'https://www.facebook.com/v3.3/dialog/oauth',
     tokenURL: 'https://graph.facebook.com/v3.3/oauth/access_token',
