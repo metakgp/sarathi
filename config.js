1d599de1ccc3833bc6c070488926043e0a0cd167
Arib Alam <aribalam64@gmail.com>
2019-06-24 14:07:47 +0530

    changed scope

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     profileURL: 'https://graph.facebook.com/v3.3/me',
     authorizationURL: 'https://www.facebook.com/v3.3/dialog/oauth',
     tokenURL: 'https://graph.facebook.com/v3.3/oauth/access_token',
-    profileFields: ['id', 'name', 'user_link'],
+    profileFields: ['id', 'name'],
     publicKey: 'BJ56v8kzMSUQpY_q0aE10hhkGIEZRljK9Vu7tZdHKQ6Br61Q_gJKxegx1pPb5zkJsd8pUjALfJmgw85yvU5PclM',
     privateKey: '7ZDzGXhbYgBRn1CG9LQ2LGoS7DbSF8G-WN0ZnFFSgTY',
 }
