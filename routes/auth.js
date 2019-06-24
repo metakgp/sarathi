2ff6e8dfa8fdc0e9be7e6bb5d8e3fec285d8fe86
Arib Alam <aribalam64@gmail.com>
2019-06-24 11:53:42 +0530

    redirect to production url

--- routes/auth.js
+++ routes/auth.js
@@ -9,8 +9,8 @@ router.get('/facebook', passport.authenticate("facebook"));
 // params - state-param : unique code to prevent csrf
 // 
 router.get('/facebook/callback', passport.authenticate("facebook", {
-  successRedirect: 'http://localhost:3000/loginRedirect',
-  failureRedirect: 'http://localhost:3000/login',
+  successRedirect: 'https://travelkgp.herokuapp.com//loginRedirect',
+  failureRedirect: 'https://travelkgp.herokuapp.com//login',
 }));
 
 router.get('/logout', (req, res) => {
