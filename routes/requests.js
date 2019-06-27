1b08504068c22c6f9d06d861b073109b4cd7afda
Arib Alam <aribalam64@gmail.com>
2019-06-28 01:17:29 +0530

    removed nested callbacks and use async await

--- routes/requests.js
+++ routes/requests.js
@@ -8,253 +8,209 @@ router.get('/join_request', (req, res) => {
   res.render('join_request.ejs');
 });
 
-// sends a join request to the owner of the group
-router.post('/join_request', (req, res) => {
-    var traveler = {
-      fb_id: req.user.fb_id,
-      profile: req.user.profile,
-      name: req.user.name,
-      from: req.body.from,
-      to: req.body.to,
-      time: new Date(req.body.time),
-    };
-  
-    // find the group the user wants to join
-    models.Group.findById(req.body.groupId).exec((err, group) => {
-      if (err)
-        res.send(500, err);
+router.post('/join_request', async (req, res) => {
 
-      var request = models.Request({
-        group: group,
-        traveler: traveler,
-      });
-  
-      // create a request object
-      request.save((err) => {
-        if (err)
-          res.send(500, err);
-        else {  
-          // add those requests to the users concerned
-          models.User.findOneAndUpdate({fb_id: req.user.fb_id}, {$push: {sent_requests: request}})
-          .exec((err, user) => {
-            if (err) {
-              res.send(500, err);
-            }
-  
-            models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, {$push: {received_requests: request}})
-            .exec((err, owner) => {
-              if (err)
-                res.send(500, err);
-              else {
-  
-                // send request notication to this user (the owner of the group)
-                const message = {
-                  icon: 'http://graph.facebook.com/' + user.fb_id + '/picture?type=square',
-                  type: 'join_request',
-                  title: 'Join Request',
-                  body: user.name + " has sent a join request",
-                };
-                
-                utils.sendNotification(owner.push_subscription, message)
-                .then(() => res.send(200)).catch(err => {console.log(err); res.send(err)});
-              }
-            });
-          });
-        }
-      });
+  try {
+    var traveler = {
+      // fb_id: req.user.fb_id,
+      // profile: req.user.profile,
+      // name: req.user.name,
+      // from: req.body.from,
+      // to: req.body.to,
+      // time: new Date(req.body.time),
+      fb_id: 12345,
+      name: "Abu Zar Ali",
+      from: new Date(),
+      to: new Date(),
+      time: new Date()
+    };
+
+    var group = await models.Group.findById(req.body.groupId).exec();
+
+    // create a request object
+    var requestModel = models.Request({
+      group: group,
+      traveler: traveler,
     });
+
+    var request = await requestModel.save();
+
+    // add those requests to the users concerned
+    var promiseArray = [];
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, {$push: {received_requests: request}}).exec()
+    );
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: traveler.fb_id}, {$push: {sent_requests: request}}).exec()
+    );
+
+    var [owner, traveler] = await Promise.all(promiseArray);
+
+    const message = {
+      icon: 'http://graph.facebook.com/' + traveler.fb_id + '/picture?type=square',
+      type: 'join_request',
+      title: 'Join Request',
+      body: traveler.name + " has requested to join your group",
+    };
+    
+    await utils.sendNotification(owner.push_subscription, message)
+    res.sendStatus(200)
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+
 });
+
 
 router.get('/approve_request', (req, res) => {
   res.render('approve_request.ejs');
 });
   
-  // approves a request to join the group
-  //TODO: update notification and send it to all members
-router.post('/approve_request', (req, res) => {
-    // find the request object
-    models.Request.findById(req.body.requestId, (err, request) => {
-      if (err)
-        res.send(500, err);
-      else {
-        // find the group and add the travler
-        models.Group.findByIdAndUpdate(request.group, {$push: {members: request.traveler}})
-        .exec((err, group) => {
-          if (err)
-            res.send(500, err);
-          else {
-            // remove received request from owner of the group
-            models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, 
-              {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
-              .exec((err) => {
-                if (err)
-                  res.send(500, err);
-                else {
-                  // remove request from sent_request of traveler
-                  models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
-                    {$pull: {sent_requests: req.body.requestId}, $push: {joined_groups: group}})  //remove requests matching req id
-                    .exec((err, traveler) => {
-                      if (err)
-                        res.send(500, err);
-                      else {
-                        
-                        // message to all the members of the group
-                        const messageToMembers = {
-                          icon: 'http://graph.facebook.com/' + traveler.fb_id + '/picture?type=square',
-                          type: 'approve_request',
-                          title: 'Group Update',
-                          body: traveler.name + " has joined the group",
-                        };
+router.post('/approve_request', async (req, res) => {
 
-                        const subjectToMembers = {
-                          fb_id: traveler.fb_id,
-                          name: traveler.name,
-                        }
-                        
-                        var promiseArray = group.members.map(item => {
-                          utils.createNotification(messageToMembers, subjectToMembers, group)
-                          .then(notification =>
-                            models.User.findOneAndUpdate({fb_id: item.fb_id},
-                            {$push: {'notifications': notification}})
-                            .exec())
-                          .then(user => utils.sendNotification(user.push_subscription, messageToMembers));
-                        });
+  try {
 
-                        // message to the traveler
-                        const messageToTraveler = {
-                          icon: 'http://graph.facebook.com/' + group.owner.fb_id + '/picture?type=square',
-                          type: 'approve_request',
-                          title: 'Request Update',
-                          body: group.owner.name + " has approved your request",
-                        };
+    var request = await models.Request.findById(req.body.requestId).exec();
+    var group = await models.Group.findByIdAndUpdate(request.group, {$push: {members: request.traveler}}).exec();
 
-                        const subjectToTraveler = {
-                          fb_id: group.owner.fb_id,
-                          name: group.owner.name,
-                        }
+    var promiseArray = [];
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, {$pull: {received_requests: req.body.requestId}}).exec()
+    );
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
+        {$pull: {sent_requests: req.body.requestId}, $push: {joined_groups: group}}).exec()
+    );
 
-                        promiseArray.push(utils.createNotification(messageToTraveler, subjectToTraveler, group)
-                        .then(notification =>
-                          models.User.findOneAndUpdate({fb_id: traveler.fb_id},
-                          {$push: {'notifications': notification}})
-                          .exec())
-                        .then(user => utils.sendNotification(user.push_subscription, messageToTraveler)));
-                
-                        Promise.all(promiseArray).then(values => res.send(200)).catch(err => {
-                          console.log(err);
-                          res.status(500).send(err);
-                        });
+    var [owner, traveler] = await Promise.all(promiseArray);
 
-                      }
-                  });
-                }
-            });
-          }
-        });
-      }
-    });
+    // message to all the members of the group
+    const messageToMembers = {
+      icon: 'http://graph.facebook.com/' + traveler.fb_id + '/picture?type=square',
+      type: 'approve_request',
+      title: 'Group Update',
+      body: traveler.name + " has joined the group",
+    };
+
+    const subjectToMembers = {
+      fb_id: traveler.fb_id,
+      name: traveler.name,
+    }
+    
+    promiseArray = group.members.map(item => 
+
+      utils.createNotification(messageToMembers, subjectToMembers, group)
+      .then(notification => models.User.findOneAndUpdate({fb_id: item.fb_id}, {$push: {'notifications': notification}}).exec())
+      .then(user => utils.sendNotification(user.push_subscription, messageToMembers))
+    
+    );
+
+    // message to the traveler
+    const messageToTraveler = {
+      icon: 'http://graph.facebook.com/' + owner.fb_id + '/picture?type=square',
+      type: 'approve_request',
+      title: 'Request Update',
+      body: owner.name + " has approved your request to join the group",
+    };
+
+    const subjectToTraveler = {
+      fb_id: owner.fb_id,
+      name: owner.name,
+    }
+
+    var promiseArray = [];
+    promiseArray.push(
+      
+      utils.createNotification(messageToTraveler, subjectToTraveler, group)
+      .then(notification => models.User.findOneAndUpdate({fb_id: traveler.fb_id}, {$push: {'notifications': notification}}).exec())
+      .then(user => utils.sendNotification(user.push_subscription, messageToTraveler)) 
+    
+    );
+
+    await Promise.all(promiseArray);
+    res.sendStatus(200);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+
 });
 
 router.get('/reject_request', (req, res) => {
   res.render('approve_request.ejs');
 });
 
-// rejects a request to join the group
-//TODO: update notification and send it to all members
-router.post('/reject_request', (req, res) => {
-    // find the request object
-    models.Request.findById(req.body.requestId, (err, request) => {
-        if (err)
-        res.send(500, err);
-        else {
-        // find the group
-        models.Group.findById(request.group)
-        .exec((err, group) => {
-            if (err)
-            res.send(500, err);
-            else {
-            // remove received request from owner of the group
-            models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, 
-                {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
-                .exec((err) => {
-                if (err)
-                  res.send(500, err);
-                else {
-                  res.send(200);
-                    // remove request from sent_request of traveler
-                    models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
-                    {$pull: {sent_requests: req.body.requestId}})  //remove requests matching req id
-                    .exec((err, traveler) => {
-                        if (err)
-                        res.send(500, err);
-                        else {
 
-                        // message to the traveler
-                        const message = {
-                            icon: 'http://graph.facebook.com/' + group.owner.fb_id + '/picture?type=square',
-                            type: 'approve_request',
-                            title: 'Request Update',
-                            body: group.owner.name + " has rejected your request",
-                        };
+router.post('/reject_request', async (req, res) => {
 
-                        const subject = {
-                          fb_id: group.owner.fb_id,
-                          name: group.owner.name,
-                        }
+  try {
+    var request = await models.Request.findById(req.body.requestId).exec();
+    var group = await models.Group.findById(request.group).exec();
 
-                        utils.createNotification(message, subject, group)
-                        .then(notification =>
-                          models.User.findOneAndUpdate({fb_id: traveler.fb_id},
-                          {$push: {'notifications': notification}})
-                          .exec())
-                        .then(user => utils.sendNotification(user.push_subscription, message));
-                        
-                      }
-                    });
-                }
-                });
-            }
-        });
-        }
-    });
+    var promiseArray = [];
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, {$pull: {received_requests: req.body.requestId}}).exec()
+    )
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, {$pull: {sent_requests: req.body.requestId}}).exec()
+    )
+
+    var [owner, traveler] = await Promise.all(promiseArray);
+
+    // message to the traveler
+    const message = {
+        icon: 'http://graph.facebook.com/' + owner.fb_id + '/picture?type=square',
+        type: 'approve_request',
+        title: 'Request Update',
+        body: owner.name + " has rejected your request to join the group",
+    };
+
+    const subject = {
+      fb_id: owner.fb_id,
+      name: owner.name,
+    }
+
+    await utils.createNotification(message, subject, group)
+    .then(notification => models.User.findOneAndUpdate({fb_id: traveler.fb_id}, {$push: {'notifications': notification}}).exec())
+    .then(user => utils.sendNotification(user.push_subscription, message))
+    
+    res.sendStatus(200)
+
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+
 });
 
-// rejects a request to join the group
-//TODO: update notification and send it to all members
-router.post('/cancel_request', (req, res) => {
-  // find the request object
-  models.Request.findById(req.body.requestId, (err, request) => {
-      if (err)
-      res.send(500, err);
-      else {
-      // find the group
-      models.Group.findById(request.group)
-      .exec((err, group) => {
-          if (err)
-          res.send(500, err);
-          else {
-          // remove received request from owner of the group
-          models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, 
-              {$pull: {received_requests: req.body.requestId}})  //remove requests matching req id
-              .exec((err) => {
-              if (err)
-                  res.send(500, err);
-              else {
-                  // remove request from sent_request of traveler
-                  models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, 
-                  {$pull: {sent_requests: req.body.requestId}})  //remove requests matching req id
-                  .exec((err, traveler) => {
-                      if (err)
-                        res.send(500, err);
-                      else
-                        res.sendStatus(200);
-                  });
-              }
-              });
-          }
-      });
-      }
-  });
+router.post('/cancel_request', async (req, res) => {
+
+  try {
+    var request = await models.Request.findById(req.body.requestId).exec();
+    var group = await models.Group.findById(request.group).exec();
+
+    var promiseArray = [];
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: group.owner.fb_id}, {$pull: {received_requests: req.body.requestId}}) 
+    );
+    promiseArray.push(
+      models.User.findOneAndUpdate({fb_id: request.traveler.fb_id}, {$pull: {sent_requests: req.body.requestId}})
+    )
+
+    await Promise.all(promiseArray);
+    res.sendStatus(200);
+    
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+
 });
+
 
 module.exports = router;
