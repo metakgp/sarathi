45cb5ff3856f60cbfa1c28c9f36f3c1b7133b398
Arib Alam <aribalam64@gmail.com>
2019-06-28 18:21:20 +0530

    added test code for icon displaying

--- client/public/service-worker-custom.js
+++ client/public/service-worker-custom.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', event => {
     const message = event.data.json();
-    
+    message.icon = 'https://travelkgp.herokuapp.com/icon.png';
     self.registration.showNotification(message.title, message);
 });
