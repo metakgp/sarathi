c9c98ba7c5195caa6d81fce5c59d846a318b8c58
Arib Alam <aribalam64@gmail.com>
2019-06-29 11:16:25 +0530

    fixed imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './searchComps/respNav'
+import Navbar from './searchComps/navBar'
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 //import { BrowserRouter as Router,Route } from 'react-router-dom';
