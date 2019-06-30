5183c1df5f21a5da8076193ad122f8edc95a527d
Arib Alam <aribalam64@gmail.com>
2019-06-30 23:13:00 +0530

    changed implementation to add secondary message

--- client/src/displays/emptyMessage.js
+++ client/src/displays/emptyMessage.js
@@ -5,12 +5,16 @@ export default function EmptyMessage(props) {
 
     return (
         <div style={{color: '#dfdfdf', margin: 20}}> 
-            <Typography 
-            variant='h2' 
-            color='inherit'
-            align='center'>
-            {props.children}
+            <Typography variant='h2' color='inherit' align='center' style={{margin: 20}}>
+            {props.primary}
             </Typography>
+            { props.secondary ?
+                <Typography variant='h4' color='inherit' align='center' style={{margin: 10}}>
+                {props.secondary}    
+                </Typography>
+            :
+                ''
+            }
         </div>
     );
 
