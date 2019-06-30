45e389d1e777a8778c7f9953049acdd2cd047a91
Arib Alam <aribalam64@gmail.com>
2019-06-30 19:46:40 +0530

    added notificationclick listener to open url

--- client/public/service-worker-custom.js
+++ client/public/service-worker-custom.js
@@ -1,5 +1,32 @@
 self.addEventListener('push', event => {
     const message = event.data.json();
-    message.icon = 'https://travelkgp.herokuapp.com/icon.png';
     self.registration.showNotification(message.title, message);
+});
+
+self.addEventListener('notificationclick', event => {
+    const urlToOpen = new URL(event.notification.urlToOpen, self.location.origin).href;
+
+    const promiseChain = clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+    })
+    .then((windowClients) => {
+    let matchingClient = null;
+
+    for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        if (windowClient.url === urlToOpen) {
+        matchingClient = windowClient;
+        break;
+        }
+    }
+
+    if (matchingClient) {
+        return matchingClient.focus();
+    } else {
+        return clients.openWindow(urlToOpen);
+    }
+    });
+
+    event.waitUntil(promiseChain)
 });
