20381f94ab6a08868b5e7ec7d67a2f6a8ec736ce
Arib Alam <aribalam64@gmail.com>
2019-06-30 23:09:09 +0530

    added invalidPage component

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,9 @@ import LoginPage from './pages/login'
 import Logout from './pages/logout'
 import privacyPolicy from './pages/privacyPolicy';
 import termsOfUse from './pages/termsOfUse';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
-import withAuth from './AuthWrapper'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import withAuth from './AuthWrapper';
+import InvalidPage from './pages/invalidPage';
 
 function LoginRedirector(props) {
     const redirectUrl = window.localStorage.getItem('redirectUrl');
@@ -23,15 +24,18 @@ class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Route path = '/login' component={LoginPage} />
-                <Route path = '/logout' component={Logout} />
-                <Route exact path="/" component={withAuth(Search)} />
-                <Route path ="/groups" component={withAuth(Groups)} />
-                <Route path='/requests' component={withAuth(Requests)} />
-                <Route path="/notifs" component={withAuth(Notifs)} />
-                <Route path='/loginRedirect' component={LoginRedirector} />
-                <Route path='/privacyPolicy' component={privacyPolicy} />
-                <Route path='/termsOfUse' component={termsOfUse} />
+                <Switch>
+                    <Route path = '/login' component={LoginPage} />
+                    <Route path = '/logout' component={Logout} />
+                    <Route exact path="/" component={withAuth(Search)} />
+                    <Route path ="/groups" component={withAuth(Groups)} />
+                    <Route path='/requests' component={withAuth(Requests)} />
+                    <Route path="/notifs" component={withAuth(Notifs)} />
+                    <Route path='/loginRedirect' component={LoginRedirector} />
+                    <Route path='/privacyPolicy' component={privacyPolicy} />
+                    <Route path='/termsOfUse' component={termsOfUse} />
+                    <Route component={InvalidPage} />
+                </Switch>
             </BrowserRouter>
         )
     }
