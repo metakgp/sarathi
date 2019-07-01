4240c7b8fb937dfb9c922070fa8b390fefa380cd
Arib Alam <aribalam64@gmail.com>
2019-07-01 17:42:53 +0530

    fetch data from server to get badge counts

--- client/src/inputs/navBar.js
+++ client/src/inputs/navBar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {AppBar, Toolbar, Link, Drawer, IconButton, List, ListItem, Badge} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import axios from 'axios';
 
 function ListItemLink(props) {
   return (
@@ -18,7 +19,11 @@ function ListItemLink(props) {
 
 export default function NavigationBar() {
   
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [groupsCount, setGroupsCount] = useState(0);
+  const [requestsCount, setRequestsCount] = useState(0);
+  const [notifCount, setNotifCount] = useState(0);
+
 
   const closeDrawer = () => {
     setOpen(false);
@@ -31,7 +36,18 @@ export default function NavigationBar() {
   const matches = useMediaQuery('(max-width: 768px)');
   
   useEffect(() => {
-    // make api calls here to get badge counts
+    async function fetchData() {
+      
+      var groups = (await axios.get('/api/user/groups_count')).data;
+      setGroupsCount(groups);
+
+      var requests = (await axios.get('/api/user/requests_count')).data;
+      setRequestsCount(requests);
+
+      var notifs = (await axios.get('/api/user/unread_notif_count')).data;
+      setNotifCount(notifs);
+    }
+    fetchData();
   }, []);
   
   return (
@@ -47,17 +63,17 @@ export default function NavigationBar() {
               </IconButton>
             :
               <div>
-              <Badge color='secondary' badgeContent={1}>
+              <Badge color='secondary' badgeContent={groupsCount}>
                     <Link variant='button' color='inherit' underline='none' style={{marginRight: 10, marginLeft: 20}} href='/groups'>
                     Groups
                     </Link>
               </Badge>
-              <Badge color='secondary' badgeContent={1}>
+              <Badge color='secondary' badgeContent={requestsCount}>
                   <Link variant='button' color='inherit' underline='none' style={{marginRight: 10, marginLeft: 20}} href='/requests'>
                   Requests
                   </Link>
               </Badge>
-              <Badge color='secondary' badgeContent={1}>
+              <Badge color='secondary' badgeContent={notifCount}>
                   <Link variant='button' color='inherit' underline='none' style={{marginRight: 10, marginLeft: 20}} href='/notifs'>
                   Notifications
                   </Link>
@@ -75,13 +91,13 @@ export default function NavigationBar() {
           onClose={closeDrawer}
         >
           <List>
-            <ListItemLink href='\groups' badgeCount={1}>
+            <ListItemLink href='\groups' badgeCount={groupsCount}>
               Groups
             </ListItemLink>
-            <ListItemLink href='\requests' badgeCount={1}>
+            <ListItemLink href='\requests' badgeCount={requestsCount}>
               Requests
             </ListItemLink>
-            <ListItemLink href='\notifs' badgeCount={0}>
+            <ListItemLink href='\notifs' badgeCount={notifCount}>
               Notifications
             </ListItemLink>
             <ListItemLink href='\logout' badgeCount={0}>
