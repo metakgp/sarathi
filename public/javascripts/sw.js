4c31c5abfa93c43db2232e1d6c119834fe55a35a
Arib Alam <aribalam64@gmail.com>
2019-07-09 00:20:25 +0530

    added assets

--- public/javascripts/sw.js
+++
@@ -1,9 +0,0 @@
-console.log("Service worker loaded");
-
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    console.log("Push received");
-    self.registration.showNotification(data.title, {
-        body: data.body,
-    });
-});
