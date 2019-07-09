b438f355a7e9538d549e30380e0958fee8183302
Arib Alam <aribalam64@gmail.com>
2019-07-09 17:41:51 +0530

    changed to development url

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    appId:367673853841637,
-    appSecret:'89d8b85e7ad8be62bef548534d6c60bb',
-    callbackUrl:'https://travelkgp.herokuapp.com/api/auth/facebook/callback',
-    // callbackUrl:'http://localhost:5000/api/auth/facebook/callback',
+    // callbackUrl:'https://travelkgp.herokuapp.com/api/auth/facebook/callback',
+    callbackUrl:'http://localhost:5000/api/auth/facebook/callback',
     profileURL: 'https://graph.facebook.com/v3.3/me',
     authorizationURL: 'https://www.facebook.com/v3.3/dialog/oauth',
     tokenURL: 'https://graph.facebook.com/v3.3/oauth/access_token',
     profileFields: ['id', 'name'],
-    publicKey: 'BJ56v8kzMSUQpY_q0aE10hhkGIEZRljK9Vu7tZdHKQ6Br61Q_gJKxegx1pPb5zkJsd8pUjALfJmgw85yvU5PclM',
-    privateKey: '7ZDzGXhbYgBRn1CG9LQ2LGoS7DbSF8G-WN0ZnFFSgTY',
 }
