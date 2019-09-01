be740de0be081f6abbf6170f272233b40cb96818
Arib Alam <aribalam64@gmail.com>
2019-09-01 12:26:00 +0530

    removed the width attribute from the footer component

--- client/src/displays/footer.js
+++ client/src/displays/footer.js
@@ -4,7 +4,7 @@ import {Typography, Link} from '@material-ui/core';
 const footer = (props) => {
 
     return (
-        <div style={{position: "fixed", left: 0, bottom: 0, backgroundColor: '#EEEEEE', width: '100%', textAlign:'center', padding: '15px'}}>
+        <div style={{position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: '#EEEEEE', textAlign:'center', padding: '1rem'}}>
             <Typography variant='body1'>
                 Contribute to this project on <Link href='https://github.com/metakgp/sarathi'>Github</Link> | Powered by metakgp with ♥
             </Typography>
