7fdc157b0f8f09ec200cb037b052385f4a118866
Arib Alam <aribalam64@gmail.com>
2019-09-01 18:54:53 +0530

    fixed the container

--- client/src/displays/footer.js
+++ client/src/displays/footer.js
@@ -4,7 +4,7 @@ import {Typography, Link} from '@material-ui/core';
 const footer = (props) => {
 
     return (
-        <div style={{position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: '#EEEEEE', textAlign:'center', padding: '1rem'}}>
+        <div id="footer" style={{position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: '#EEEEEE', textAlign:'center', padding: '1rem'}}>
             <Typography variant='body1'>
                 Contribute to this project on <Link href='https://github.com/metakgp/sarathi'>Github</Link> | 
                 Powered by <Link href='https://metakgp.github.io/'>metakgp</Link> with ♥
