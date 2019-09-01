c32f63b111be7e71b8189b34d2f4bd98b89aa457
Arib Alam <aribalam64@gmail.com>
2019-09-01 13:14:16 +0530

    added metakgp link to footer

--- client/src/displays/footer.js
+++ client/src/displays/footer.js
@@ -6,7 +6,8 @@ const footer = (props) => {
     return (
         <div style={{position: "fixed", left: 0, right: 0, bottom: 0, backgroundColor: '#EEEEEE', textAlign:'center', padding: '1rem'}}>
             <Typography variant='body1'>
-                Contribute to this project on <Link href='https://github.com/metakgp/sarathi'>Github</Link> | Powered by metakgp with ♥
+                Contribute to this project on <Link href='https://github.com/metakgp/sarathi'>Github</Link> | 
+                Powered by <Link href='https://metakgp.github.io/'>metakgp</Link> with ♥
             </Typography>
         </div>
     )
