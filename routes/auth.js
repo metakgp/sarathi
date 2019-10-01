dc7fc4333763d317544a379438f220bf9ef8de55
Arib Alam <aribalam64@gmail.com>
2019-10-02 04:39:39 +0530

    apssport to generate web tokens on successfull login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var jwt = require('jsonwebtoken')
 
 // send an authentication request to facebook OAuth
 router.get('/facebook', passport.authenticate("facebook"));
@@ -9,9 +10,16 @@ router.get('/facebook', passport.authenticate("facebook"));
 // params - state-param : unique code to prevent csrf
 // 
 router.get('/facebook/callback', passport.authenticate("facebook", {
-  successRedirect: 'http://localhost:3000/',
-  failureRedirect: 'http://localhost:3000/login'
-}));
+  failureRedirect: 'http://localhost:3000/login',
+  session: false
+}), (req, res) => {
+
+  // generate web tokens
+  var token = jwt.sign({fb_id: req.user.fb_id}, process.env.jwtSecret || 'thisismysecret', {expiresIn: '1h'});
+
+  // redirect to success page with token as url parameter
+  res.redirect('http://localhost:3000/loginRedirect?' + token);
+});
 
 router.get('/logout', (req, res) => {
   req.logout();
@@ -22,13 +30,6 @@ router.get('/login', (req, res) => {
   res.render('login.ejs', {user: req.user});
 });
 
-router.get('/status', (req, res) => {
-  if (req.user) {
-    res.status(200).send("OK");
-  }
-  else {
-    res.status(500).send("Err");
-  }
-});
+
 
 module.exports = router;
