1932ab32b72ee8389784f24e297bf6c8a3e919dc
Arib Alam <aribalam64@gmail.com>
2019-10-02 23:39:08 +0530

    removed backend code

--- app.js
+++ app.js
@@ -1,106 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
-var fbStrategy = require('passport-facebook').Strategy;
-var webpush = require('web-push');
-var cors = require('cors');
-var axios = require('axios');
-var fs = require('fs');
-
-var groupRouter = require('./routes/group');
-var authRouter = require('./routes/auth');
-var indexRouter = require('./routes/index');
-var requestsRouter = require('./routes/requests');
-var userRouter = require('./routes/user');
-var staticPageRouter = express.Router();
-
-var config = require('./config');
-var models = require('./models/index').models;
-
+var router = express.Router();
 
 var app = express();
-
-passport.serializeUser((user, done) => {
-  done(null, user.fb_id);
-});
-
-passport.deserializeUser((fb_id, done) => {
-  models.User.findOne({fb_id: fb_id}, (err, user) => {
-    if (err)
-      done(err);
-    done(null, user);
-  });
-});
-
-passport.use(new fbStrategy({
-  clientID: process.env.appId || config.appId,
-  clientSecret: process.env.appSecret || config.appSecret,
-  callbackURL: config.callbackUrl,
-  profileURL: config.profileURL,
-  profileFields: config.profileFields,
-}, (token, refreshToken, profile, done) => {
-  models.User.findOne({fb_id: profile.id}).exec((err, user) => {
-    if (err) {
-      console.log("Error searching for user");
-      return done(err);
-    }
-    
-    if (!user) {
-      // create and save the user model
-      var newUser = models.User({
-        name: profile.name.givenName + " " + profile.name.familyName,
-        fb_id: profile.id,
-        token: token,
-        profile: profile.profileUrl,
-      });
-      newUser.save((err, object) => {
-        if (err) {
-          console.log("Error creating new user");
-          return done(err);
-        }
-        console.log("Successfully created new user");
-        return done(null, object);
-      });
-    }
-    else {
-      console.log("User found. Returning the user");
-      return done(null, user);
-    }
-  });
-}));
-
-webpush.setVapidDetails(
-  'mailto:aribalam64@gmail.com',
-  process.env.publicKey || config.publicKey,
-  process.env.privateKey || config.privateKey
-);
-
-
-if (process.env.NODE_ENV === 'production') {
-  staticPageRouter.use((req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
-}
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(session({secret: 'app-secret', resave: true, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Setting static media directory and including service worker option
 const options = {
@@ -108,28 +11,23 @@ const options = {
     res.set('Service-Worker-Allowed', '/');
   },
 };
-app.use(express.static(path.join(__dirname, 'public'), options));
 
-// use the build folder created by react app when in production
-if (process.env.NODE_ENV === 'production')
-  app.use(express.static('client/build'));
+// Include the build folder when in production
+if (process.env.NODE_ENV === 'production') {
 
-// function isLoggedIn(req, res, next) {
-//   if  (req.path === '/api/auth/login' || req.isAuthenticated())
-//     return next();
-//   res.sendStatus(403);
-// }
+  app.use(express.static('client/build'), options);
+  router.use((req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
 
-// app.use(isLoggedIn);
+}
+else {
+  router.get('/', (req, res) => {
+    res.send("You are in development mode. Move to client/ folder to start React Server");
+  })
+}
 
-app.use('/api/auth', authRouter);
-app.use('/api/group', groupRouter);
-app.use('/api/user', userRouter);
-app.use('/api/request', requestsRouter);
-app.use('/api', indexRouter);
-
-
-app.use('/', staticPageRouter);
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -146,5 +44,9 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500).send(err);
 });
+
+const port = process.env.port || 8000;
+
+app.listen(port);
 
 module.exports = app;
