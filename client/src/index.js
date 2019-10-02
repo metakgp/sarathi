9910b3bd76c71876154825fe376f8111d4aa1abb
Arib Alam <aribalam64@gmail.com>
2019-10-02 16:45:38 +0530

    add interceptors

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,15 @@ import Navbar from './inputs/navBar'
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import Footer from './displays/footer'
+import axios from 'axios';
+import { join } from 'path';
 //import { BrowserRouter as Router,Route } from 'react-router-dom';
 // import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 
+axios.interceptors.request.use((config) => {
+    config.url = join('https://sarathi-api.herokuapp.com', config.url);
+    return config;
+});
 
 ReactDOM.render(<Navbar />, document.getElementById('navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
